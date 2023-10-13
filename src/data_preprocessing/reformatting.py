011af512fd051559a3384261b28055ab7211e607
Estelle Chabanel <eec42@login1.grace.ycrc.yale.edu>
2023-10-13 09:58:26 -0400

    update paths for data formatting

--- src/data_preprocessing/reformatting.py
+++ src/data_preprocessing/reformatting.py
@@ -23,15 +23,17 @@ for dataset in config.keys():
     dataset_config = extract_dataset_config(config, dataset)
     print(dataset_config)
 
+    # Source dataset folder
+    source_dataset_folder = os.path.join(data_folder, original_dataset_folder, dataset_config["name"])
+    if not os.path.exists(source_dataset_folder):
+        continue
+
     # Create new folder to store current dataset
     dataset_folder = os.path.join(data_folder, saving_dataset_folder, dataset_config["name"])
     if not os.path.exists(dataset_folder):
         os.mkdir(dataset_folder)
         os.mkdir(os.path.join(dataset_folder, "image"))
         os.mkdir(os.path.join(dataset_folder, "label"))
-
-    # Source dataset folder
-    source_dataset_folder = os.path.join(data_folder, original_dataset_folder, dataset_config["name"])
 
     category_name_to_id = eval(dataset_config["formatting_method"])(dataset_config, source_dataset_folder, dataset_folder, category_name_to_id)
     preview_few_images(dataset_config, source_dataset_folder, dataset_folder, category_name_to_id)
