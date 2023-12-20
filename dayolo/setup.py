34459c721982ca27ac50e983272a6bbcfb3e8d92
Estelle Chabanel <eec42@r903u37n01.grace.ycrc.yale.edu>
2023-12-20 09:14:25 -0500

    new package name

---
+++ dayolo/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='dayolo',
+      version='0.1',
+      # description='The funniest joke in the world',
+      # url='http://github.com/storborg/funniest',
+      # author='Flying Circus',
+      # author_email='ee.chabanel@gmail.com',
+      # license='MIT',
+      packages=['dayolo'],
+      # zip_safe=False
+)
